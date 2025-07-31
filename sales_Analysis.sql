2b2c7b9f88e761edb610583f720ef9ab9487c0b4
Sesethu_Bango <bsesethu@yahoo.com>
2025-07-31 18:38:39 +0200

    Phase 3 Complete

--- sales_Analysis.sql
+++ sales_Analysis.sql
@@ -31,6 +31,7 @@ FROM (
 	)c
 GROUP BY category
 ORDER BY sum_revenue DESC
+-------------------------------------------------------------------------------------------------------
 
 -- Customer Insights
 	-- Who are the top 10 customers by lifetime value?
@@ -49,8 +50,7 @@ GROUP BY c.customer_id, c.name, c.email
 ORDER BY sum_total DESC
 
 	--What percentage of customers made more than one order?
-SELECT * FROM CUSTOMERS c INNER JOIN ORDERS o ON c.customer_id = o.customer_id
-
+--SELECT * FROM CUSTOMERS c INNER JOIN ORDERS o ON c.customer_id = o.customer_id -- Joined tables
 SELECT count_1plus / 200.0 * 100 percnt_of_cust_more_than_one_order
 FROM (
 	SELECT
@@ -66,4 +66,64 @@ FROM (
 		)t
 	WHERE count_id > 1
 	)s
+---------------------------------------------------------------------------------------------------------
 
+-- Product Performance
+	--What are the top 5 best-selling products per category?
+-- SELECT * FROM PRODUCTS p INNER JOIN ORDER_ITEMS oi ON p.product_id = oi.product_id
+-- SELECT * FROM ORDER_ITEMS
+
+SELECT TOP 5
+	product_id,
+	name,
+	category,
+	SUM(revenue) sum_revenue
+FROM (
+	SELECT
+		p.product_id,
+		p.name,
+		p.category,
+		CAST(oi.quantity as INT) Quantity_int,
+		CAST(REPLACE(oi.price, '$', '') as FLOAT) price_float,
+		CAST(oi.quantity as INT) * CAST(REPLACE(oi.price, '$', '') as FLOAT) revenue -- quantity * price
+	FROM PRODUCTS p 
+	INNER JOIN ORDER_ITEMS oi 
+	ON p.product_id = oi.product_id
+	)p
+GROUP BY product_id, name, category
+ORDER BY sum_revenue DESC
+	
+	--Which products have high stock but poor sales?
+SELECT TOP 15
+	product_id,
+	name,
+	category,
+	quantity_stock,
+	SUM(revenue) sum_revenue
+FROM (
+	SELECT
+		p.product_id,
+		p.name,
+		p.category,
+		p.stock_quantity * 50 quantity_stock, -- Make the quanities more resonable
+		CAST(oi.quantity as INT) quantity_sold_int,
+		CAST(REPLACE(oi.price, '$', '') as FLOAT) price_float,
+		CAST(oi.quantity as INT) * CAST(REPLACE(oi.price, '$', '') as FLOAT) revenue -- quantity * price
+	FROM PRODUCTS p 
+	INNER JOIN ORDER_ITEMS oi 
+	ON p.product_id = oi.product_id
+	)p
+WHERE quantity_stock > 1000 --AND sum_revenue < 1000
+GROUP BY product_id, name, category, quantity_stock 
+ORDER BY sum_revenue 
+-- poor sum_revenue is values below 1000
+-- high stock is values above 1300
+-- These are bad selling products with a high stock count
+-------------------------------------------------------------------------------------------------------------------
+
+-- Operational & Data Quality
+	-- Are there any orders where total_amount doesn’t match the sum of items?
+
+	
+	
+	--Identify duplicate emails in the customer table.
